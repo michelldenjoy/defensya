b81255cf44b1556623eaf033a43d17f5166b9e94
michelldenjoy <michelldenjoy@gmail.com>
2026-03-26 10:45:40 +0100

    fixing deploy problem in abouthero

--- app/empresa/quienes-somos/page.tsx
+++ app/empresa/quienes-somos/page.tsx
@@ -7,7 +7,7 @@ export default function page() {
   return (
     <div>
       <HeroAbout
-        label="Empresa"
+        
         title="Quiénes somos"
         subtitle="Defensya es una empresa líder en el sector de defensa y seguridad, con más de 25 años de experiencia en el diseño, desarrollo e integración de sistemas avanzados para clientes militares y civiles en todo el mundo."
         image="/logodefensyanew.png"
